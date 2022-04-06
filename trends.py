e98f6a48725d5103b570283aa1dd9869933bdb30
Hopper <Glenn.Hopper@signetjewelers.com>
2022-04-06 14:01:03 -0400

    Added the TrendLine class, testing coefficient and offsets to get ready for optimization

--- trends.py
+++ trends.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plot
 
 # hl = hosting language
 # tz = timezone
-pytrends = TrendReq(hl='en-US', tz=360) 
-
-
-
-
+pytrends = TrendReq(hl='en-US', tz=360)
 
 keywords = ["family circus", "pearls before swine", "beetle bailey"]
 keywords = ["diamonds", "opal", "rubies", "gold", "silver"]
@@ -15,24 +11,41 @@ keywords = ["diamonds", "opal", "rubies"]
 keywords = ["10K rings", "14K rings"]
 keywords = ["zales", "kay"]
 
-timeframe = '2005-01-01 2007-01-01'
+timeframe = '2004-01-01 2008-01-01'
 # timeframe = '2020-06-01 2020-06-07'
 
 class TrendLine(object):
+    '''
+    Contains the keyword, the datapoints, the coefficient and the offset.
+    '''
     def __init__(self, keyword, datapoints):
         self.keyword = keyword
         self.datapoints = datapoints
         self.coefficient = 1
         self.offset = 0
     def __getitem__(self, i):
-        return self.coefficient self.datapoints[i] + self.offset
+        return self.coefficient * self.datapoints[i] + self.offset
+    def __iter__(self):
+        for datapoint in self.datapoints:
+            yield self.coefficient * datapoint + self.offset
     def __len__(self):
         return len(self.datapoints)
     def fit_line_to(self, other_trendline):
-        pass # TODO
+        pass # TODO: the holy grail of this whole thing
+    def difference_between(self, other_trendline):
+        total = 0
+        for i, datapoint in enumerate(self):
+            total += pow((datapoint - other_trendline[i]), 2)
+        average = total / len(self)
+        return average # TODO
     def plot(self):
-        # TODO
-        plot.plot(range(len(self)), self.datapoints, label = self.keyword)
+        plot.plot(range(len(self)), self, label = self.keyword)
+
+class TrendCollection(object): # TODO
+    def __init__(self, trendlines):
+        self.trendlines = trendlines
+    def plot(self):
+        pass # TODO
 
 def normalized_trends(keywords):
     # plots the normalized trend lines,
@@ -45,17 +58,32 @@ def normalized_trends(keywords):
         data = pytrends.interest_over_time()
         data = data.reset_index()
         interest = list(data[keyword])[:-1] # trim ending zero (why is there a zero here?)
-        y_data[keyword] = interest
+        y_data[keyword] = TrendLine(keyword, interest)
         print(interest)
-        plot.plot(range(len(interest)), interest, label = keyword)
+        y_data[keyword].plot()
+    print('difference: ', y_data['zales'].difference_between(y_data['kay']))
     plot.legend()
     plot.show()
     return y_data
 
-normalized_trends(keywords)
+trends = normalized_trends(keywords)
+first_keyword = list(trends.keys())[0]
+second_keyword = list(trends.keys())[1]
+trends[first_keyword].offset = 35
+trends[first_keyword].coefficient = 0.3
+for trend in trends:
+    trends[trend].plot()
+print('difference: ', trends[second_keyword].difference_between(trends[first_keyword]))
+plot.show()
 
-
-
+'''
+trends = normalized_trends(keywords)
+trends['zales'].offset = 40
+trends['zales'].coefficient = 0.5
+for trend in trends:
+    trends[trend].plot()
+plot.show()
+'''
 '''
 pytrends.build_payload(keywords, cat = 0, timeframe = timeframe)
 
